ff5930f7b2e7afe6ff2bbb094fb5c2a935b6e02d
zhangkuixun <1129826190@qq.com>
2018-05-30 17:57:16 +0800

    no message

--- app/src/main/java/com/kun/dagger2rxjava2retrofit2demo/activities/main/tet.java
+++
@@ -1,8 +0,0 @@
-package com.kun.dagger2rxjava2retrofit2demo.activities.main;
-
-/**
- * Created by ${KevinZhang} on 2018/5/29.
- */
-
-public class tet {
-}
--- baselib/src/main/java/com/kun/baselib/base/BaseResponse.java
+++ baselib/src/main/java/com/kun/baselib/base/BaseResponse.java
@@ -8,7 +8,7 @@ import com.google.gson.annotations.Expose;
  */
 public class BaseResponse<T> {
 
-    @Expose
+    @Expose//Expose区分实体中不想被序列化的属性，其自身包含两个属性deserialize(反序列化)和serialize（序列化），默认都为true。
     private String message;
     @Expose
     private int status;
